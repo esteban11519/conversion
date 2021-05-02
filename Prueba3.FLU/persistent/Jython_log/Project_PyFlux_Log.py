8db4520ef9ebdefc24a389877af19d6b82081b3c
jfiagar <jfiagar@unal.edu.co>
2021-05-02 14:41:16 -0500

    .

--- Prueba3.FLU/persistent/Jython_log/Project_PyFlux_Log.py
+++ Prueba3.FLU/persistent/Jython_log/Project_PyFlux_Log.py
@@ -33,3 +33,96 @@ restoreCircuitLinks()
 
 saveProject()
 
+exportCircuit(filename='Prueba3_log3.xcir')
+
+storeCircuitLinks()
+
+DeleteElectricCircuit(deleteStrandedConductor='YES')
+
+importCircuit(filename='Prueba3_log3.xcir',
+              importValue=1,
+              ImportFELink=1)
+
+
+restoreCircuitLinks()
+
+exportCircuit(filename='Prueba3_log4.xcir')
+
+storeCircuitLinks()
+
+DeleteElectricCircuit(deleteStrandedConductor='YES')
+
+importCircuit(filename='Prueba3_log4.xcir',
+              importValue=1,
+              ImportFELink=1)
+
+
+restoreCircuitLinks()
+
+RegionFace['PHASE_NEG_1'].magneticTransient2D=MagneticTransient2DFaceCoilConductor(coilConductor=CoilConductor2DNegative(turnNumber='66',
+                                                                                                                         seriesParallel=AllInSeries(),
+                                                                                                                         electricComponent=CoilConductor['COILCONDUCTOR_1']),
+                                                                                   material=Material['FLU_COPPER'])
+
+
+RegionFace['PHASE_NEG_2'].magneticTransient2D=MagneticTransient2DFaceCoilConductor(coilConductor=CoilConductor2DNegative(turnNumber='66',
+                                                                                                                         seriesParallel=AllInSeries(),
+                                                                                                                         electricComponent=CoilConductor['COILCONDUCTOR_2']),
+                                                                                   material=Material['FLU_COPPER'])
+
+
+RegionFace['PHASE_NEG_3'].magneticTransient2D=MagneticTransient2DFaceCoilConductor(coilConductor=CoilConductor2DNegative(turnNumber='66',
+                                                                                                                         seriesParallel=AllInSeries(),
+                                                                                                                         electricComponent=CoilConductor['COILCONDUCTOR_3']),
+                                                                                   material=Material['FLU_COPPER'])
+
+
+RegionFace['PHASE_POS_1'].magneticTransient2D=MagneticTransient2DFaceCoilConductor(coilConductor=CoilConductor2DPositive(turnNumber='66',
+                                                                                                                         seriesParallel=AllInSeries(),
+                                                                                                                         electricComponent=CoilConductor['COILCONDUCTOR_1']),
+                                                                                   material=Material['FLU_COPPER'])
+
+
+RegionFace['PHASE_POS_2'].magneticTransient2D=MagneticTransient2DFaceCoilConductor(coilConductor=CoilConductor2DPositive(turnNumber='66',
+                                                                                                                         seriesParallel=AllInSeries(),
+                                                                                                                         electricComponent=CoilConductor['COILCONDUCTOR_1']),
+                                                                                   material=Material['FLU_COPPER'])
+
+
+RegionFace['PHASE_POS_2'].magneticTransient2D=MagneticTransient2DFaceCoilConductor(coilConductor=CoilConductor2DPositive(turnNumber='66',
+                                                                                                                         seriesParallel=AllInSeries(),
+                                                                                                                         electricComponent=CoilConductor['COILCONDUCTOR_2']),
+                                                                                   material=Material['FLU_COPPER'])
+
+
+RegionFace['PHASE_POS_3'].magneticTransient2D=MagneticTransient2DFaceCoilConductor(coilConductor=CoilConductor2DPositive(turnNumber='66',
+                                                                                                                         seriesParallel=AllInSeries(),
+                                                                                                                         electricComponent=CoilConductor['COILCONDUCTOR_3']),
+                                                                                   material=Material['FLU_COPPER'])
+
+
+Scenario(name='Scenario_1')
+
+startMacroTransaction()
+
+Scenario['Scenario_1'].addPilot(pilot=MultiValues(parameter=VariationParameter['ANGPOS_ROTOR'],
+                                                  intervals=[IntervalLogStepNumber(minValue=1.0E-7,
+                                                                                   maxValue=1.0,
+                                                                                   stepNumber=30)]))
+
+endMacroTransaction()
+
+Scenario['SCENARIO_1'].solve(projectName='G:/Mi unidad/UNIVERSIDAD/Octavo Semestre/Conversion electromagnetica/SIMULACION PROYECTO FINAL/Prueba3.FLU')
+
+DeleteAllResults(deletePostprocessingResults='yes')
+
+checkMesh()
+
+checkCircuit()
+
+result = checkGeometry()
+
+result = checkPhysic()
+
+saveProject()
+
